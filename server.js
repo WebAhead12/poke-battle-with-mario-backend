ea34eb6b299181252978166221a3749c5387a7e2
Diabolica <scripter.Diabolica@gmail.com>
2021-12-02 11:11:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const cors = require("cors");
 const PORT = process.env.PORT || 4000;
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use(router);
 
 app.listen(PORT, () => {
